151a6e6f4d09f3d14083a18d369ebe0cee3b20e5
许钊瑞 <xuzhaorui.yx@qq.com>
2025-08-12 16:26:50 +0800

    Add constructor annotation to Question

--- src/main/java/com/eduplatform/corefunctionprototype/entity/Question.java
+++ src/main/java/com/eduplatform/corefunctionprototype/entity/Question.java
@@ -1,13 +1,16 @@
 package com.eduplatform.corefunctionprototype.entity;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 /**
  * @author xzr
  */
 @Data
+@AllArgsConstructor
 public class Question {
     private Long id;
     private String content;
     private String answer;
+
 }
