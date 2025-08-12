e950882f711630a4b4a1a30c33303fd2bb7a17aa
许钊瑞 <xuzhaorui.yx@qq.com>
2025-08-12 16:20:51 +0800

    Add Question entity and repository

---
+++ src/main/java/com/eduplatform/corefunctionprototype/entity/Question.java
@@ -0,0 +1,13 @@
+package com.eduplatform.corefunctionprototype.entity;
+
+import lombok.Data;
+
+/**
+ * @author xzr
+ */
+@Data
+public class Question {
+    private Long id;
+    private String content;
+    private String answer;
+}
