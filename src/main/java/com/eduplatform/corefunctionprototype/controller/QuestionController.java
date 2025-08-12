deb273193c6ef8a9197535718b55c435eb484580
许钊瑞 <xuzhaorui.yx@qq.com>
2025-08-12 16:21:39 +0800

    Implement Question REST controller

---
+++ src/main/java/com/eduplatform/corefunctionprototype/controller/QuestionController.java
@@ -0,0 +1,13 @@
+package com.eduplatform.corefunctionprototype.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+/**
+ * REST controller for managing questions in the educational platform.
+ * Provides endpoints to create and retrieve questions.
+ */
+@RestController
+@RequestMapping("/questions")
+public class QuestionController {
+
+}
